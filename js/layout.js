799fa47b27bef75e226371b7579c74d132a32838
intensity <intensity.web@gmail.com>
2015-01-01 21:18:30 +0200

    css refactored, removed some useless js, and refactored last comments widget

--- js/layout.js
+++ js/layout.js
@@ -1,31 +1,5 @@
 $(document).ready(function () {
-
     $('#delbutton').click(function () {
         confirm("Ви дійсно хочете видалити даний контент?");
-    });
-
-    function hideAllDropdowns() {
-        $(".dropped .drop-menu-main-sub").hide();
-        $(".dropped").removeClass('dropped');
-        $(".dropped .drop-menu-main-sub .title").unbind("click");
-    }
-
-    function showDropdown(el) {
-        var el_li = $(el).parent().addClass('dropped');
-        el_li
-            .find('.title')
-            .click(function () {
-                hideAllDropdowns();
-            })
-            .html($(el).html());
-        el_li.find('.drop-menu-main-sub').show();
-    }
-
-    $(".drop-down").click(function () {
-        showDropdown(this);
-    });
-
-    $(document).mouseup(function () {
-        hideAllDropdowns();
     });
 });
--- protected/components/widgets/views/MyLastComments/default.php
+++ protected/components/widgets/views/MyLastComments/default.php
@@ -1,24 +1,28 @@
-<style type="text/css">
-    #com {
-        border-bottom: 1px solid silver;
-    }
+<div class="content"
+    <div class="col-md-4" id="comments_replace">
+        <div class="detailBox">
+            <div class="titleBox">
+                <label>Останні коментарі</label>
+            </div>
+            <div class="actionBox">
+                <ul class="commentList">
+                    <?php foreach ($comments as $coment) { ?>
+                        <li>
+                            <div class="commenterImage">
+                                <img
+                                    src="<?php echo Yii::app()->request->baseUrl; ?>/images/profile/<?php echo $coment->author->profile->photo; ?>"
+                                    width="80" height="80" class="img-circle" id="profile_comments_avatar" alt=""/>
+                            </div>
+                            <div class="commentText">
+                                <p><?php echo $coment->content; ?></p>
 
-    #com #author {
-        float: right;
-    }
-</style>
-<h3>Останні коментарі</h3>
-<span>
-<?php
-foreach ($comments as $coment):
-    ?>
-    <div id="comment">
-        <span id="dt"><? echo date('G:i:s ,j.n.Y', $coment->date); ?></span>
-        <span id="author"><? echo $coment->author; ?></span>
-
-        <p><? echo $coment->content; ?></p>
-
+                                <p><?php echo $coment->date; ?></p>
+                                By: <?php echo $coment->author->login; ?>
+                            </div>
+                        </li>
+                    <?php } ?>
+                </ul>
+            </div>
+        </div>
     </div>
-
-<?php endforeach; ?>
-</span>
+</div>
