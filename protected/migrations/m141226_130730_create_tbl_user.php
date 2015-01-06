83f595a47ac18b84ccd0ef1f96ac6a721cb43538
intensity <intensity.web@gmail.com>
2015-01-06 14:38:39 +0200

    migrations foreign key issue solved

--- protected/migrations/m141226_130730_create_tbl_user.php
+++ protected/migrations/m141226_130730_create_tbl_user.php
@@ -5,7 +5,7 @@ class m141226_130730_create_tbl_user extends CDbMigration
     public function up()
     {
         $this->createTable('tbl_user', array(
-            'id'       => 'INT(10) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY',
+            'id'       => 'pk',
             'login'    => 'VARCHAR(255) NOT NULL',
             'password' => 'VARCHAR(255) NOT NULL',
             'email'    => 'VARCHAR(255) NOT NULL',
--- protected/migrations/m141227_075410_create_tbl_profile.php
+++ protected/migrations/m141227_075410_create_tbl_profile.php
@@ -10,10 +10,10 @@ class m141227_075410_create_tbl_profile extends CDbMigration
             'info'       => 'VARCHAR(255) NOT NULL',
             'birth'      => 'VARCHAR(255) NOT NULL',
             'registered' => 'DATETIME NOT NULL',
-            'user_id'    => 'INT(10) DEFAULT NULL',
+            'user_id'    => 'INT(10) NOT NULL',
             'ban'        => 'TINYINT(10)',
         ));
-//        $this->addForeignKey('user_profile', 'tbl_profile', 'user_id', 'tbl_user', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('user_profile', 'tbl_profile', 'user_id', 'tbl_user', 'id', 'CASCADE', 'CASCADE');
     }
 
     public function down()
