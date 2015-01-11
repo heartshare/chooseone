1c1ddebc4c8cf5e970783622fc0f7732e603d069
intensity <intensity.web@gmail.com>
2015-01-11 20:52:34 +0200

    start work on creating math captcha

---
+++ protected/components/MathCaptchaAction.php
@@ -0,0 +1,26 @@
+<?php
+
+class MathCaptchaAction extends CCaptchaAction
+{
+    public function generateVerifyCode()
+    {
+        return mt_rand((int)$this->minLength, (int)$this->maxLength);
+    }
+
+    public function renderImage($code)
+    {
+        parent::renderImage($this->getText($code));
+    }
+
+    protected function getText($code)
+    {
+        $code = (int)$code;
+        $rand = mt_rand(1, $code - 1);
+        $op = mt_rand(0, 1);
+        if ($op) {
+            return $code - $rand . "+" . $rand;
+        } else {
+            return $code + $rand . "-" . $rand;
+        }
+    }
+} 
