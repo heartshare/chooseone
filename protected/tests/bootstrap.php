18079fae6502923120abe7a92b434687f61b64d9
NovikovViktor <viktor.novikov95@gmail.com>
2015-02-20 15:15:49 +0200

    some work on test running

--- protected/tests/bootstrap.php
+++ protected/tests/bootstrap.php
@@ -1,10 +1,10 @@
 <?php
+$_SERVER['SCRIPT_FILENAME'] = dirname(__FILE__) . '/../../index-test.php';
+$_SERVER['SCRIPT_NAME'] = basename($_SERVER['SCRIPT_FILENAME']);
 
 // change the following paths if necessary
-$yiit=dirname(__FILE__).'/../../../YiiRoot/framework/yiit.php';
-$config=dirname(__FILE__).'/../config/test.php';
-
+$yiit = dirname(__FILE__) . '/../../vendor/yiisoft/yii/framework/yiit.php';
+$config = dirname(__FILE__) . '/../config/test.php';
 require_once($yiit);
-require_once(dirname(__FILE__).'/WebTestCase.php');
-
+require_once(dirname(__FILE__) . '/WebTestCase.php');
 Yii::createWebApplication($config);
